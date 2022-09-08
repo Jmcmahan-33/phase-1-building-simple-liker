72b7bfc1849bd86817f00d7dc7050494f0ffbb48
Jared McMahan <jaredm788@gmail.com>
2022-09-08 13:03:58 -0600

    commit tree dd802ec418bc4a79cfa277812a1213df1f57a266

--- main.js
+++ main.js
@@ -1,10 +1,46 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+const hideError = document.querySelector("#modal")
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+console.log("hi")
+hideError.classList.add("hidden")
 
+  
+  clickLike()
+})
+
+function errorMessage() {
+  hideError.classList.add("hidden")
+
+}
+
+function clickLike() {
+  document.addEventListener("click", (e) => {
+    if(e.target.classList[0] === "like-glyph") {
+      mimicServerCall()
+      .then((response) => {
+        const activated = e.target.classList.contains("activated-heart") 
+        if(activated) {
+          e.target.classList.remove("activated-heart");
+          e.target.classList.innerHTML = EMPTY_HEART
+        }else{
+          e.target.classList.add("activated-heart")
+          e.target.classList.innerHTML = FULL_HEART
+        }
+        activated
+      })
+      .catch(error => {
+        hideError.remove("hidden")
+        setTimeout(() => {
+          errorMessage()
+        },3000)
+      })
+    }
+  })
+}
 
 
 //------------------------------------------------------------------------------
